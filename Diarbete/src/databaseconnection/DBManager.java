f54f28447559673b3c38f0f2faa13efe8275e441
DESKTOP-9V60HSP\Francesco <theforce@hotmail.it>
2017-01-29 14:38:40 +0100

    aggiunta connessione al database

---
+++ Diarbete/src/databaseconnection/DBManager.java
@@ -0,0 +1,32 @@
+package databaseconnection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+public class DBManager {
+
+	private static DBManager manager=new DBManager();
+	private BasicDataSource datasource;
+	
+	private DBManager(){
+		datasource=new BasicDataSource();
+		datasource.setDriverClassName("com.mysql.jdbc.Driver");
+		datasource.setUrl("jdbc:mysql://localhost/diarbete");
+		datasource.setUsername("root");
+		datasource.setPassword("");
+		datasource.setMaxTotal(100);
+		datasource.setInitialSize(1);
+		datasource.setMaxIdle(20);
+		datasource.setMaxWaitMillis(60000);
+	}
+	
+	public Connection getConnection() throws SQLException{
+		return datasource.getConnection();
+	}
+	
+	public static DBManager getInstance(){
+		return manager;
+	}
+}
