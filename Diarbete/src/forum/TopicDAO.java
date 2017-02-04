ae8392813a46ae3636c0ecf6b47b79da6fc6cb66
DESKTOP-9V60HSP\Francesco <theforce@hotmail.it>
2017-02-04 12:34:23 +0100

    Modifiche

--- Diarbete/src/forum/TopicDAO.java
+++ Diarbete/src/forum/TopicDAO.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import forum.Risposta;
 import databaseconnection.DBManager;
 import eccezioni.ParametroIllegaleException;
 import utenti.TipologieUtenti;
